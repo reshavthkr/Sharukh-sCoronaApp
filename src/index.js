5e8255ddf4a4e59cffcf977378ad76d405ba9657
Reshav Thakur <Reshavthkr@gmail.com>
2021-05-23 13:40:41 +0530

    added serve file in public folder

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import tachyons from 'tachyons';
 import {BrowserRouter} from 'react-router-dom';
-
 import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -17,4 +18,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
